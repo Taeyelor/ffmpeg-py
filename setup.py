89c45297e9c0d7c3fecaf3f74f16eb92c18015f5
taeyelor <taeyelor@gmail.com>
2022-08-14 14:38:35 +0430

    Change setup details and fix import issue.

--- setup.py
+++ setup.py
@@ -24,21 +24,6 @@ setup(
     long_description=dedent(r"""\
      FFmpeg wrapper for Python
         """),
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Text Processing :: Markup",
-        "Topic :: Multimedia :: Video",
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License"
-    ],
     python_requires=">=3.7",
     install_requires=requires
 )
